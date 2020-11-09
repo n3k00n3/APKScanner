05c69c7800c6b2ea8a3d576a574d32eb8be7e3d3
n3k00n3 <n3k00n3@protonmail.com>
2020-11-09 09:39:02 -0300

    added more information into readme

--- APKScanner.sh
+++ APKScanner.sh
@@ -58,7 +58,8 @@ function checkPermissions() {
 }
 
 # Debugable
-# Debugging was enabled on the app which makes it easier for reverse engineers to hook a debugger to it. This allows dumping a stack trace and accessing debugging helper classes.
+# Debugging was enabled on the app which makes it easier for reverse engineers to hook a debugger to it.
+# This allows dumping a stack trace and accessing debugging helper classes.
 function checkDebug() {
 	echo "[+] Debugable App"
 	echo "-----------------"
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # APKScanner
-Simple APK scanner 
+
+Simple APK Static scanner
+
+The objective of this scanner is to find for misconfiguration, sensitive data and insecure components.
+
+
+### Dependencies 
+
+You need to have jadx tool installed and in your PATH.
+
+[Jadx](https://github.com/skylot/jadx)
